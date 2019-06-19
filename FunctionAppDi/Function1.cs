e3657b2d56c6ef76d85c191f84bc9cd303b68f72
Gavin Lang <gavin.lang@health.telstra.com>
2019-06-19 20:29:17 +1000

    output greeting to a servicebus

--- FunctionAppDi/Function1.cs
+++ FunctionAppDi/Function1.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using System.IO;
@@ -14,16 +15,19 @@ namespace FunctionAppDi
     {
         private readonly ILogger _log;
         private readonly IGuidService _guidService;
+        private readonly IConfiguration _cfg;
 
-        public Function1(ILogger<Function1> log, IGuidService guidService)
+        public Function1(ILogger<Function1> log, IGuidService guidService, IConfiguration cfg)
         {
             _log = log;
             _guidService = guidService;
+            _cfg = cfg;
         }
 
         [FunctionName("DependentFunction")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [ServiceBus("scratch", Connection = "SbConnection")]IAsyncCollector<string> queueCollector)
         {
             _log.LogInformation("Information");
             _log.LogTrace("Trace");
@@ -37,10 +41,13 @@ namespace FunctionAppDi
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            var greeting = _cfg["Greeting"] ?? "Hello";
             _log.LogInformation("Guid Service says {guid}", _guidService.NewGuid());
-            _log.LogInformation("Hello {name}!", name);
+            _log.LogInformation("{greeting}, {name}!", greeting, name);
+            await queueCollector.AddAsync($"{greeting}, {name}!");
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name}")
+                ? (ActionResult)new OkObjectResult($"{greeting}, {name}!")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
--- FunctionAppDi/Startup.cs
+++ FunctionAppDi/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Di.Lib.Service;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.IO;
 
 [assembly: FunctionsStartup(typeof(FunctionAppDi.Startup))]
 
@@ -10,7 +12,14 @@ namespace FunctionAppDi
     {
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("local.settings.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+
             builder.Services
+                .AddSingleton<IConfiguration>(config)
                 .AddSingleton<IGuidService, GuidService>()
                 .AddLogging();
             ;
